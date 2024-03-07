1e7d20547f8feb684358629de9c56cef58840fa4
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 19:36:20 +0200

    Create TaskListView in views.py

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
+from django.views import generic
 
-# Create your views here.
+from todo_list.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    ordering = ("is_completed", "-created_at")
+
+    def get_queryset(self) -> QuerySet:
+        task_qs = Task.objects.all().prefetch_related("tags")
+        if self.get_ordering():
+            return task_qs.order_by(*self.get_ordering())
+        return task_qs
