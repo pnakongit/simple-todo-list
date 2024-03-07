be3e6d2dc71d0a46655e6608e200c53091a2268b
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 19:37:14 +0200

    Add new path wiht name index to urls.py

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
-urlpatterns = []
+from todo_list.views import TaskListView
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="index"),
+]
 
 app_name = "todo_list"
