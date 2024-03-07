d15a492b78d77839a1f72e9a244d3133f76ba4ad
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 19:57:18 +0200

    Add new url path with name task_delete to urls.py

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 from todo_list.views import (
     TaskListView,
     TaskCreateView,
-    TaskUpdateView
+    TaskUpdateView,
+    TaskDeleteView
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task_create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
 ]
 
 app_name = "todo_list"
