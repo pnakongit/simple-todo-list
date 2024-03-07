856b468c38df166899e530353941a6a27614b07d
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 19:52:24 +0200

    Add new url path with name task_update to urls.py

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from todo_list.views import TaskListView, TaskCreateView
+from todo_list.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView
+)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task_create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
 ]
 
 app_name = "todo_list"
