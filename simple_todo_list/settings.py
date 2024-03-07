60d66a5e72014cf569033deaf8c2e97529c124e0
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 19:46:23 +0200

    Add required settings for django crispy form

--- simple_todo_list/settings.py
+++ simple_todo_list/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "todo_list",
+    "crispy_forms",
+    "crispy_bootstrap4",
+
 ]
 
 MIDDLEWARE = [
@@ -116,3 +119,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
