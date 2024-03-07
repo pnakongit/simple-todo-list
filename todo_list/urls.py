2a134d0f085810bab03656e2566e2e6dec8aaccf
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 19:48:41 +0200

    Add new url path with name task_create in urls.py

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from todo_list.views import TaskListView
+from todo_list.views import TaskListView, TaskCreateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task_create"),
 ]
 
 app_name = "todo_list"
