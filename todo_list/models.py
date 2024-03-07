725cd4d0e496a2fc63db05b475429a5e56980f82
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 19:20:07 +0200

    Add string representation in Task&Tag models

--- todo_list/models.py
+++ todo_list/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return f"{self.pk} {self.name}"
+
 
 class Task(models.Model):
     content = models.CharField(max_length=255)
@@ -15,3 +18,6 @@ class Task(models.Model):
         related_name="tags",
         blank=True
     )
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.content}"
